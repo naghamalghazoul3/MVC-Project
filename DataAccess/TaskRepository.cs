9086b35022b51fd512aa9a58ef47184b21df128f
naghamalghazoul3 <alghazoulnagham@gmail.com>
2026-06-03 23:56:34 +0300

    First commit without delete function

---
+++ DataAccess/TaskRepository.cs
@@ -0,0 +1,39 @@
+using System.Collections.Generic;
+using System.Linq;
+using NTierTodoApp.Models;
+
+namespace NTierTodoApp.DataAccess
+{
+    /// <summary>
+    /// مستودع البيانات لإدارة المهام باستخدام قائمة في الذاكرة.
+    /// </summary>
+    public class TaskRepository
+    {
+        private List<TaskItem> tasks = new List<TaskItem>
+        {
+            new TaskItem { Id = 1, Title = "مهمة أولى", IsComplete = false },
+            new TaskItem { Id = 2, Title = "مهمة ثانية", IsComplete = false }
+        };
+
+        public List<TaskItem> GetAll() => tasks;
+
+        public void Add(TaskItem task)
+        {
+            // هنا نضيف المهمة إلى القائمة.
+            tasks.Add(task);
+        }
+
+        public TaskItem GetById(int id)
+        {
+            return tasks.FirstOrDefault(t => t.Id == id);
+        }
+
+
+        public void Delete(int id) 
+        { 
+            // TODO: ابحث عن المهمة باستخدام id 
+ 
+            // TODO:  إذا كانت المهمة موجودة، قم بإزالتها من القائمة 
+        } 
+    }
+}
---
+++ Web/Controllers/HomeController.cs
@@ -0,0 +1,56 @@
+using Microsoft.AspNetCore.Mvc;
+using NTierTodoApp.Business;
+
+namespace NTierTodoApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly TaskService taskService;
+
+        public HomeController(TaskService service)
+        {
+            taskService = service;
+        }
+
+        /// <summary>
+        /// يعرض قائمة المهام.
+        /// </summary>
+        public IActionResult Index()
+        {
+            var tasks = taskService.GetTasks();
+            return View(tasks);
+        }
+
+        /// <summary>
+        /// إضافة مهمة جديدة.
+        /// </summary>
+        [HttpPost]
+        public IActionResult AddTask(string title)
+        {
+            if (!string.IsNullOrWhiteSpace(title))
+                taskService.AddTask(title);
+            return RedirectToAction("Index");
+        }
+
+        /// <summary>
+        /// تعيين مهمة كمكتملة.
+        /// </summary>
+        [HttpPost]
+        public IActionResult CompleteTask(int id)
+        {
+            taskService.CompleteTask(id);
+            return RedirectToAction("Index");
+        }
+
+        /// <summary>
+        /// حذف مهمة.
+        /// </summary>
+      [HttpPost] 
+        public IActionResult DeleteTask(int id) 
+        { 
+            // TODO: استدعاء دالة حذف المهمة في TaskService 
+ 
+            return RedirectToAction("Index"); 
+        } 
+    }
+}
